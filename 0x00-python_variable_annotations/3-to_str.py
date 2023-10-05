0c4d15fa23bca1000d0b58d6383a13983fd56a74
Epicsociety <measkswho@gmail.com>
2023-10-05 10:21:21 +0300

    type-annotated function takes a float arg and return a str

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+
+"""
+Utilize type annotations
+"""
+
+
+def to_str(n: float) -> str:
+    """
+    Functions receives arguements of type float
+    Return: string representation of the arg
+    """
+
+    return str(n)
