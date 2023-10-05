38052d447fac2d3c88f10bb028e64b9e5d6c99ac
Epicsociety <measkswho@gmail.com>
2023-10-05 10:00:48 +0300

    a type-annotated function that takes arg of str and return a str

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Utilize type notations
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Functions receives arguements of type str
+    Return: a concatenated string
+    """
+
+    return (str1 + str2)
