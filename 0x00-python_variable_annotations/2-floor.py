0da82ff123325129ecc40fdeacebbb8d78feada9
Epicsociety <measkswho@gmail.com>
2023-10-05 10:10:23 +0300

    type-annotated function that takes a float and return a floor of the float

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import math
+
+"""
+Utilize type annotations
+"""
+
+
+def floor(n: float) -> int:
+    """
+    Functions receives arguements of type float
+    Return: floor area: type int
+    """
+
+    return math.floor(n)
