e5de74026bb3b2ca176fa05412ad9e4d981601fa
Epicsociety <measkswho@gmail.com>
2023-10-05 09:53:33 +0300

    simple function with type annotations

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+"""
+Utilize type notations
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Functions receives arguements of type int
+    Return: a float
+    """
+
+    return (a + b)
