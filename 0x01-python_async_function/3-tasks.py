afb6d16767e12b2536793b1a2ef420d2aafd890e
Epicsociety <measkswho@gmail.com>
2023-10-09 21:25:26 +0300

    returns asyncio.Task

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -5,11 +5,11 @@ Module contains an asynchronous coroutine
 
 
 import asyncio
-from typing import Awaitable
+from typing import Coroutine
 wait_random = __import__("0-basic_async_syntax").wait_random
 
 
-def task_wait_random(max_delay: int) -> Awaitable:
+def task_wait_random(max_delay: int) -> Coroutine:
     """
     returns asyncio.Task
     Args:
