21425fc28c483f3a36fd55b0f6b82ad5d7718225
Epicsociety <measkswho@gmail.com>
2023-10-11 22:20:14 +0300

    collect 10 random numbers using an async comprehensing,     then return the 10 random numbers

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Implementation of Async Comprehension
+"""
+
+
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> float:
+    """
+    collect 10 random numbers using an async comprehensing,
+    then return the 10 random numbers
+    """
+
+    return [i async for i in async_generator()]
