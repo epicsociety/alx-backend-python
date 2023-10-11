3dd62856ae539e012c6951186d26b89886a8a795
Epicsociety <measkswho@gmail.com>
2023-10-11 22:01:37 +0300

    oop 10 times, each time asynchronously wait 1 second, then yield a random number between 0 and 10

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Implementation of Async Comprehension
+"""
+
+
+from typing import Generator
+import asyncio
+import random
+
+
+async def async_generator() -> Generator[int, None, None]:
+    """
+    oop 10 times, each time asynchronously wait 1 second,
+    then yield a random number between 0 and 10
+    args: None
+    Return: None
+    Yield an int
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
