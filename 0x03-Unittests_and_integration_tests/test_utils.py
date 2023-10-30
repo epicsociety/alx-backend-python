e518d55d60c9c43571cca7f6576246e08663f770
Epicsociety <measkswho@gmail.com>
2023-10-30 12:02:18 +0300

    AssertRaises with parameterized

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,6 +8,7 @@ import unittest
 from typing import (
     Mapping,
     Sequence,
+    Any,
 )
 from parameterized import parameterized
 access_nested_map = __import__("utils").access_nested_map
@@ -21,6 +22,15 @@ class TestAccessNestedMap(unittest.TestCase):
         ({"a": {"b": 2}}, ("a", "b"), 2),
     ])
     def test_access_nested_map(self, map: Mapping, path: Sequence,
-                               nested_map: Mapping) -> bool:
+                               nested_map: Any) -> bool:
         """ The testing method """
         self.assertEqual(access_nested_map(map, path), nested_map)
+
+    @parameterized.expand([
+        ({}, ("a",)),
+        ({"a": 1}, ("a", "b")),
+    ])
+    def test_access_nested_map_exception(self, map: Mapping, path: Sequence):
+        """ Testing with asssertRaises"""
+        with self.assertRaises(KeyError):
+            access_nested_map(map, path)
